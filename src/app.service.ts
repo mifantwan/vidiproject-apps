feffea37be2b524d2052498f5b1c38506686c9fb
mifantwan <mifan@vidiproject.com>
2022-07-02 01:41:06 +0700

    vidiproject-app[staging] : unit test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Developmet Test';
+    return 'Hello World! Development';
   }
 }
