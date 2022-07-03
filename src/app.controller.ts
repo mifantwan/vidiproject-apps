b9b5e72e90c8b90934f8c9ea459676eb9237dd07
mifantwan <mifan@vidiproject.com>
2022-07-04 00:24:54 +0700

    vidiproject-app[staging] : hosts test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller({
-  host: 'staging.vidiproject.app'
-})
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
