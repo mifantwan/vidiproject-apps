c41eb5f5fa634e4bbeaccfe8e24e284fcef7ddbe
mifantwan <mifan@vidiproject.com>
2022-07-05 23:23:58 +0700

    vidiproject-app[staging] : swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Vidiproject Apps')
+    .setDescription('REST API Docs')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors();
   app.use(helmet());
   await app.listen(process.env.HOST_PORT);
